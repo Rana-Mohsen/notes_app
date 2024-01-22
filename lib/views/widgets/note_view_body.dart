f1e71bff36355cc3be2a239bfd7bf373db60674c
Rana-3r2m <rana3r2m@gmail.com>
2024-01-22 12:26:03 +0200

    custom note item

--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
 
 import 'custom_app_bar.dart';
 
@@ -12,7 +13,8 @@ class NoteViewBody extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: 38,),
-            CustomAppBar()
+            CustomAppBar(),
+            NoteItem()
           ],
         ),
       );
