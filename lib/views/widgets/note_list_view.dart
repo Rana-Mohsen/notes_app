a02855d3ae2cea841291181f9d20bedcc9aefc08
Rana-3r2m <rana3r2m@gmail.com>
2024-01-23 13:56:11 +0200

    add modal bottom sheet

---
+++ lib/views/widgets/note_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/note_item.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: NoteItem(),
+        );
+      }),
+    );
+  }
+}
--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/note_item.dart';
+import 'package:notes_app/views/widgets/note_list_view.dart';
 
 import 'custom_app_bar.dart';
 
@@ -8,13 +9,13 @@ class NoteViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+    return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
             SizedBox(height: 38,),
             CustomAppBar(),
-            NoteItem()
+            Expanded(child: NoteListView())
           ],
         ),
       );
