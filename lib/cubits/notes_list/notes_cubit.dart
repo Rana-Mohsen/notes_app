bf2417fe80b57d05a32e5106b45f14c898205610
Rana-3r2m <rana3r2m@gmail.com>
2024-07-07 15:56:52 +0300

    create fetch noe method

---
+++ lib/cubits/notes_list/notes_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+  
+  fetchNote() async{
+    var noteBox = Hive.box<NoteModel>(kNoteBox);
+    try { // future
+      emit(NoteSuccess(noteBox.values.toList()));
+    } catch (e) {
+      emit(NoteFailure(e.toString()));
+    }
+  }
+}
